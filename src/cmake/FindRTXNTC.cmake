c2b310e9b29551660d776bc71ce1dd7b41baf261
Maven Kim <mavenkim2@gmail.com>
2026-03-11 15:20:55 -0400

    build(cmake): support explicit rtxntc artifact paths

--- src/cmake/FindRTXNTC.cmake
+++ src/cmake/FindRTXNTC.cmake
@@ -1,20 +1,37 @@
 include(FindPackageHandleStandardArgs)
 
 set(LIBNTC_ROOT "" CACHE PATH "Path to a prebuilt LibNTC installation")
+set(LIBNTC_INCLUDE_DIR "" CACHE PATH "Path to the LibNTC include directory")
+set(LIBNTC_LIBRARY "" CACHE FILEPATH "Path to the prebuilt libntc library")
 
 set(_RTXNTC_ROOT_HINTS
     ${LIBNTC_ROOT}
     $ENV{LIBNTC_ROOT})
 
-find_path(RTXNTC_INCLUDE_DIR
-    NAMES libntc/ntc.h
-    HINTS ${_RTXNTC_ROOT_HINTS}
-    PATH_SUFFIXES include)
+if (LIBNTC_INCLUDE_DIR)
+    find_path(RTXNTC_INCLUDE_DIR
+        NAMES libntc/ntc.h
+        HINTS "${LIBNTC_INCLUDE_DIR}"
+        NO_DEFAULT_PATH)
+else()
+    find_path(RTXNTC_INCLUDE_DIR
+        NAMES libntc/ntc.h
+        HINTS ${_RTXNTC_ROOT_HINTS}
+        PATH_SUFFIXES include)
+endif()
 
-find_library(RTXNTC_LIBRARY
-    NAMES libntc ntc
-    HINTS ${_RTXNTC_ROOT_HINTS}
-    PATH_SUFFIXES lib lib64 bin)
+if (LIBNTC_LIBRARY)
+    if (EXISTS "${LIBNTC_LIBRARY}" AND NOT IS_DIRECTORY "${LIBNTC_LIBRARY}")
+        set(RTXNTC_LIBRARY "${LIBNTC_LIBRARY}")
+    else()
+        set(RTXNTC_LIBRARY "RTXNTC_LIBRARY-NOTFOUND")
+    endif()
+else()
+    find_library(RTXNTC_LIBRARY
+        NAMES libntc ntc
+        HINTS ${_RTXNTC_ROOT_HINTS}
+        PATH_SUFFIXES lib lib64 bin)
+endif()
 
 find_package_handle_standard_args(RTXNTC
     REQUIRED_VARS RTXNTC_INCLUDE_DIR RTXNTC_LIBRARY)
